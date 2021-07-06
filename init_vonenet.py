47b72c1876576ae0a8ca7cc95435bbe5d0bc874e
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-06 02:53:00 +0200

    fix logger

--- init_vonenet.py
+++ init_vonenet.py
@@ -2,7 +2,7 @@
 @nrp.Robot2Neuron()
 def init_vonenet(t, visual_extractor):
     if visual_extractor.value is None:
-        clientLogger.info(f'INITIALIZATION OF THE VARIABLE {visual_extractor}')
+        clientLogger.info('INITIALIZATION OF THE VARIABLE visual_extractor')
         
         import site, os
         # WARNING: the path can change according to the python version you chose when initializing the virtualenv
